242f6298f0ca3e10edf528c3ff4b25df9a511333
Roman Acevedo <roman.acevedo62@gmail.com>
2024-06-08 13:50:00 +0200

    fix: workout history warning about keys

--- src/app/history/workout-history.tsx
+++ src/app/history/workout-history.tsx
@@ -18,39 +18,37 @@ export default function WorkoutHistory(props: WorkoutHistoryProps) {
     return (
         <div>
             <h3 className="text-center">last workouts</h3>
-            {
-                workoutList.map((workout, _i) => (
-                    <div>
-                        <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
-                            <li className="py-3 sm:py-4">
-                                <div className="flex items-center space-x-4">
+            <ul role="list" className="divide-y divide-gray-200 dark:divide-gray-700">
+                {
+                    workoutList.map((workout, _i) => (
+                        <li key={workout.id} className="py-3 sm:py-4">
+                            <div className="flex items-center space-x-4">
 
-                                    <div className="flex-shrink-0">
-                                    </div>
-                                    <div className="inline-flex min-w-0">
-                                        {workout.value.exercise}
-                                    </div>
-                                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                                        {workout.value.reps}
-                                    </div>
-                                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                                        x
-                                    </div>
-                                    <div className="inline-flex items-center text-base font-semibold text-gray-900">
-                                        {workout.value.weight}
-                                        <p className="text-gray-500 text-sm">kg</p>
-                                    </div>
-                                    <div>
-                                        <RemoveWorkoutButton
-                                            onClick={() => onDelete(workout.id)}>
-                                        </RemoveWorkoutButton>
-                                    </div>
+                                <div className="flex-shrink-0">
                                 </div>
-                            </li>
-                        </ul>
-                    </div>
-                ))
-            }
+                                <div className="inline-flex min-w-0">
+                                    {workout.value.exercise}
+                                </div>
+                                <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                                    {workout.value.reps}
+                                </div>
+                                <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                                    x
+                                </div>
+                                <div className="inline-flex items-center text-base font-semibold text-gray-900">
+                                    {workout.value.weight}
+                                    <p className="text-gray-500 text-sm">kg</p>
+                                </div>
+                                <div>
+                                    <RemoveWorkoutButton
+                                        onClick={() => onDelete(workout.id)}>
+                                    </RemoveWorkoutButton>
+                                </div>
+                            </div>
+                        </li>
+                    ))
+                }
+            </ul>
         </div>
     );
 }
