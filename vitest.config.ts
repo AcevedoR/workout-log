f19cacc704eb238aae9892704206b529cd836904
Roman Acevedo <roman.acevedo62@gmail.com>
2024-07-21 18:53:19 +0200

    fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import {configDefaults, defineConfig} from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
@@ -6,6 +6,10 @@ export default defineConfig({
     plugins: [react()],
     test: {
         environment: 'jsdom',
+        exclude:[
+            ...configDefaults.exclude,
+            'src/e2e-tests'
+        ]
     },
     resolve:{
         alias: [{ find: "@", replacement: resolve(__dirname, "./src") }]
