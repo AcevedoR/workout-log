2049b712290816e1f36f4f2a3c868f6b3078110d
Roman Acevedo <roman.acevedo62@gmail.com>
2024-07-28 09:33:57 +0200

    log

--- src/app/FeaturesConfiguration.ts
+++ src/app/FeaturesConfiguration.ts
@@ -1,4 +1,5 @@
 export const isDevModeEnabled: boolean = process.env.NODE_ENV && process.env.NODE_ENV != 'production';
+console.log("process.env.NODE_ENV: ", process.env.NODE_ENV);
 
 export const appShortDescription = "A simple app to help you log your workout sessions";
 export const appDevelopmentInformations = "Hey, this minimalistic app is still in early development, the code source is (until further notice) open source and available here: https://github.com/AcevedoR/workout-log";
