5253ccbfa32432da257834d86f84d0c3cd69ab41
Roman Acevedo <roman.acevedo62@gmail.com>
2024-08-07 10:35:21 +0200

    wip 2

--- apps/exercise-statistics/src/ExerciseStatisticsRepository.ts
+++ apps/exercise-statistics/src/ExerciseStatisticsRepository.ts
@@ -1,6 +1,5 @@
 import {UsualLift} from "../../workout-log/src/app/model/usual-lift";
 import {UserID} from "../../workout-log/src/app/UserID";
-import {Workout, WorkoutRow} from "../../workout-log/src/app/workout";
 import {getFirestore} from "firebase-admin/firestore";
 import {ExerciseStatistics} from "../../workout-log/src/app/firestore/exercise-statistics";
 
@@ -8,24 +7,26 @@ const EXERCISE_STATISTICS_COLLECTION = "exercise-statistics";
 
 export async function upsertExerciseStatistics(input: { userId: UserID, usualLift: UsualLift }): Promise<void> {
     try {
+        const data: ExerciseStatistics = {
+            _updatedDate: Date.now(),
+            exercise: input.usualLift.exercise,
+            usualLift: {
+                reps: input.usualLift.reps,
+                weight: input.usualLift.weight
+            }
+        }
         const collectionReference = getFirestore().collection(EXERCISE_STATISTICS_COLLECTION);
-        let exerciseStatisticsAlreadyExisting = await collectionReference
-            .where("userId", "==", input.userId)
-            .where("exercise", "==", input.usualLift.exercise).get();
-
-        if (exerciseStatisticsAlreadyExisting.size > 1) {
-            throw new Error("Critical error, there should only be one ExerciseStatistic row for an User and an Exercise");
-        }
-
-        if (exerciseStatisticsAlreadyExisting.empty) {
-        } else {
-            exerciseStatisticsAlreadyExisting.forEach(doc => {
-                const exerciseStatistics = doc.data() as ExerciseStatistics
-                // TODO I stopped here
-            })
-        }
+        await collectionReference
+            .doc(getExerciseStatisticsId(input.userId, input.usualLift))
+            .set(
+                data
+            )
     } catch
         (e: any) {
         console.log('Unsuccessful' + e)
     }
+}
+
+function getExerciseStatisticsId(userId: UserID, usualLift: UsualLift): string {
+    return `${userId}-${usualLift.exercise}`;
 }
