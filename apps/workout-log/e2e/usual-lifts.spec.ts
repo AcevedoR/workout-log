e4790cd622df822b7467e68085c2e7d33a158c8a
Roman Acevedo <roman.acevedo62@gmail.com>
2024-08-12 11:18:20 +0200

    test(e2e): user was not active anymore

--- apps/workout-log/e2e/usual-lifts.spec.ts
+++ apps/workout-log/e2e/usual-lifts.spec.ts
@@ -1,22 +1,41 @@
 import {expect, test} from '@playwright/test'
 import {HomePageHelper} from "./home-page-helper";
 
+// TODO clear data in beforeEach
+
 test('should display a Usual lift', async ({page}) => {
-    // TODO clear data in beforeEach
+    await test.step('Given Dev user was active today', async () => {
+        await fetch('http://127.0.0.1:9099/identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCeV1giwlkriJuW3MU-w1jHQCf0NpJHvy0',
+            {
+                method: 'POST',
+                body: JSON.stringify({
+                    "email": "dev-mode-fake-user@toto.com",
+                    "password": "dev-mode-fake-user"
+                }),
+                headers: {
+                    "content-type":"application/json"
+                }
+            })
+    });
 
     const homePage = new HomePageHelper(page);
-    await homePage.goto();
+    await test.step('When Dev user enters a workout', async () => {
+        await homePage.goto();
 
-    await homePage.submitWorkoutLog({exercise: 'Other', reps: 19, weight: 47});
-    await homePage.submitWorkoutLog({exercise: 'Squat', reps: 10, weight: 60});
-    await homePage.submitWorkoutLog({exercise: 'Squat', reps: 6, weight: 80});
-    await homePage.submitWorkoutLog({exercise: 'Squat', reps: 5, weight: 80});
-    await homePage.submitWorkoutLog({exercise: 'Squat', reps: 4, weight: 80});
+        await homePage.submitWorkoutLog({exercise: 'Other', reps: 19, weight: 47});
+        await homePage.submitWorkoutLog({exercise: 'Squat', reps: 10, weight: 60});
+        await homePage.submitWorkoutLog({exercise: 'Squat', reps: 6, weight: 80});
+        await homePage.submitWorkoutLog({exercise: 'Squat', reps: 5, weight: 80});
+        await homePage.submitWorkoutLog({exercise: 'Squat', reps: 4, weight: 80});
+    });
 
-    await fetch('http://127.0.0.1:5001/workout-log-424900/us-central1/calculate').catch(error => console.error(error))
+    await test.step('And he waits a day for the statistics to be calculated', async () => {
+        await fetch('http://127.0.0.1:5001/workout-log-424900/us-central1/calculate')
+    });
 
-    await homePage.page.reload();
+    await test.step('Then, the next time he go into the app, the UsualLift for this move was calculated', async () => {
+        await homePage.page.reload();
 
-    await expect(homePage.usualLift()).toBeVisible({timeout: 5000});
-    console.log("debug locator: ", homePage.usualLift());
+        await expect(homePage.usualLift()).toBeVisible({timeout: 5000});
+    });
 })
