2a42b6bca1ec99cb0217290751d8898913e27b65
Roman Acevedo <roman.acevedo62@gmail.com>
2024-10-02 13:42:47 +0200

    feat: add exercise progression charts

---
+++ apps/workout-log/src/app/history/exercise-progression-chart.tsx
@@ -0,0 +1,136 @@
+'use client'
+
+
+import React from "react";
+import {Workout} from "../workout";
+import {CartesianGrid, Legend, Line, LineChart, Tooltip, XAxis, YAxis} from "recharts";
+
+export interface ExerciseProgressionChartProps {
+    workoutList: Workout[]
+}
+
+export interface ExerciseWeight {
+    date: string,
+    weight: number
+}
+
+export default function ExerciseProgressionChart(props: ExerciseProgressionChartProps) {
+    const {workoutList} = props;
+    const exerciseWeights: ExerciseWeight[] = workoutList
+        .sort((a, b) => a.date - b.date)
+        .map(x => {
+                return {
+                    date: new Date(x.date).toLocaleDateString(),
+                    weight: x.weight
+                };
+            }
+        );
+
+    const maxWeightBySession: ExerciseWeight[] = Object.entries(Object.groupBy(exerciseWeights, ({date}) => date))
+        .map(([date, exerciseWeights]) => {
+            if (!exerciseWeights) {
+                throw new Error("when calculating the max weight of a session, we should always have weights");
+            }
+            return {date: date, weight: Math.max(...exerciseWeights.map(x => x.weight))}
+        });
+
+
+    function getMostDoneWeight(weights: number[]): number | null {
+        const weightOccurences: Record<number, number> = weights.reduce((acc: Record<number, number>, num: number) => {
+            acc[num] = (acc[num] || 0) + 1;
+            return acc;
+        }, {});
+
+        let w = Object.entries(weightOccurences)
+            .filter(([weight, occurence]) => occurence > 2)
+            .sort((a, b) => b[1] - a[1])
+            .sort((a, b) => Number(b[0]) - Number(a[0]));
+        if (!w || w.length == 0) {
+            return null
+        } else {
+            return Number(w[0][0]);
+        }
+    }
+
+    let weigthsForSessionWithMoreThan3Series = Object.entries(Object.groupBy(exerciseWeights, ({date}) => date))
+        .filter(([date, exerciseWeights]) => exerciseWeights && exerciseWeights.length >= 3);
+    const usualTrainingWeightBySession: ExerciseWeight[] = weigthsForSessionWithMoreThan3Series
+        .map(([date, exerciseWeights]) => {
+            if(!exerciseWeights){
+                throw new Error("when calculating the max weight of a session, we should always have weights");
+            }
+            return {date: date, weight: getMostDoneWeight(exerciseWeights.map(x => x.weight))} as ExerciseWeight
+        }).filter(exerciseWeight => exerciseWeight.weight != null);
+
+    console.log(usualTrainingWeightBySession);
+    return (
+        <div>
+            <div>
+                <h2>All data</h2>
+                {/*<ResponsiveContainer width="100%" height="100%"> TODO make work*/}
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={exerciseWeights}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <XAxis dataKey="date"/>
+                    <YAxis/>
+                    <Tooltip/>
+                    <Legend/>
+                    <Line type="monotone" dataKey="weight" stroke="#8884d8" activeDot={{r: 8}}/>
+                </LineChart>
+                {/*</ResponsiveContainer>*/}
+            </div>
+            <div>
+                <h2>Max Weight by session</h2>
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={maxWeightBySession}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <XAxis dataKey="date"/>
+                    <YAxis/>
+                    <Tooltip/>
+                    <Legend/>
+                    <Line type="monotone" dataKey="weight" stroke="#8884d8" activeDot={{r: 8}}/>
+                </LineChart>
+            </div>
+            <div>
+                <h2>Usual Weight by session</h2>
+                <LineChart
+                    width={500}
+                    height={300}
+                    data={usualTrainingWeightBySession}
+                    margin={{
+                        top: 5,
+                        right: 30,
+                        left: 20,
+                        bottom: 5,
+                    }}
+                >
+                    <CartesianGrid strokeDasharray="3 3"/>
+                    <XAxis dataKey="date"/>
+                    <YAxis/>
+                    <Tooltip/>
+                    <Legend/>
+                    <Line type="monotone" dataKey="weight" stroke="#8884d8" activeDot={{r: 8}}/>
+                </LineChart>
+            </div>
+        </div>
+    );
+
+}
--- apps/workout-log/src/app/history/workout-history-page.tsx
+++ apps/workout-log/src/app/history/workout-history-page.tsx
@@ -1,12 +1,13 @@
 'use client'
 
 
-import WorkoutShortHistory from "./workout-short-history";
 import {Firestore} from "@firebase/firestore";
 import {UserID} from "../UserID";
 import {getMostRecents} from "../firestore/WorkoutFirestore";
 import React, {useEffect, useState} from "react";
 import {WorkoutRow} from "../workout";
+import ExerciseProgressionChart from "./exercise-progression-chart";
+import WorkoutShortHistory from "./workout-short-history";
 
 export interface WorkoutHistoryPageProps {
     userID: UserID
@@ -33,15 +34,19 @@ export default function WorkoutHistoryPage(props: WorkoutHistoryPageProps) {
 
         <div className="mt-4">
             <h1 className="text-center  text-2xl">Exercise: {exercice}</h1>
-            {workoutRecentHistory.length > 0 ?
-                <WorkoutShortHistory workoutList={workoutRecentHistory}></WorkoutShortHistory>
-                : <div>No workout found</div>
-            }
             <button
                 onClick={() => setDisplayWorkoutHistoryPage(false)}
                 className="rounded-md bg-main px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-main/90 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 go back
             </button>
+            {workoutRecentHistory.length > 0 ?
+                <div>
+                    <ExerciseProgressionChart workoutList={workoutRecentHistory.map(x => x.value)}/>
+                    <WorkoutShortHistory workoutList={workoutRecentHistory}></WorkoutShortHistory>
+                </div>
+                : <div>No workout found</div>
+            }
         </div>
-    );
+    )
+        ;
 }
